ad90b489c60ec474447443ad2e731563d9262e01
yohannis <yohannisalex@gmail.com>
2021-09-10 22:17:31 +0300

    my second committtt

--- client/public/sw.js
+++ client/public/sw.js
@@ -26,7 +26,20 @@ function openPushNotification(event) {
 self.addEventListener("push", receivePushNotification);
 self.addEventListener("notificationclick", openPushNotification); */
 console.log("Service Worker Loaded...");
-
+var CACHE_NAME = 'my-pwa-cache-v1';
+var urlsToCache = [
+  '/',
+  '/tenantscreen',
+  '/src/components/screen/SwiperSlider.js',
+  '/src/components/screen/TenantScreen.js',
+  '/src/components/screen/RelatedHouses.js',
+  '/src/components/screen/HouseDetails.js',
+  '/src/components/screen/FileUploads.js',
+  '/src/components/screen/ContractForm.js',
+  '/src/components/screen/HouseInTheSameDistrict.js',
+  '/src/components/pages/Home.js'
+  
+];
 self.addEventListener("push", e => {
   const { image, tag, url, title, text } = e.data.json();
 
@@ -52,3 +65,45 @@ self.addEventListener("notificationclick", event=>{
   event.notification.close();
   event.waitUntil(clients.openWindow(event.notification.data));
 })
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        // Open a cache and cache our files
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+self.addEventListener('activate', (event) => {
+  event.waitUntil((async () => {
+    // Enable navigation preload if it's supported.
+    // See https://developers.google.com/web/updates/2017/02/navigation-preload
+    if ('navigationPreload' in self.registration) {
+      await self.registration.navigationPreload.enable();
+    }
+  })());
+
+  // Tell the active service worker to take control of the page immediately.
+  self.clients.claim();
+});
+/* self.addEventListener('fetch', function(event) {
+  console.log(event.request.url);
+  event.respondWith(
+      caches.match(event.request).then(function(response) {
+          return response || fetch(event.request);
+      })
+  );
+}); */
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open('my-pwa-cache-v1').then(function(cache) {
+      return cache.match(event.request).then(function (response) {
+        return response || fetch(event.request).then(function(response) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
--- client/src/components/screen/LandlordAccountReview.js
+++ client/src/components/screen/LandlordAccountReview.js
@@ -1,9 +1,32 @@
 import React from 'react'
-
+import { Grid,Icon,Divider,Button, } from 'semantic-ui-react'
 const LandlordAccountReview = () => {
     return (
-        <div style={{minHeight:"80vh"}}>
-            hello my beloved landlord
+        <div style={{minHeight:"100vh"}}>
+            <Grid>
+        <Grid.Column width={3}>
+         siome jlfjlj
+        </Grid.Column>
+
+
+        <Grid.Column width='13'>
+        
+          <Grid style={{ marginTop: "20px" }} verticalAlign='middle'>
+            <Grid.Column floated='left' width={13}>
+              My Listing
+            </Grid.Column>
+            <Grid.Column floated='right' width={3}>
+              <Button>
+                <Icon name='plus' />
+                Add Listing
+              </Button>
+            </Grid.Column>
+          </Grid>
+          <Divider />
+         
+        </Grid.Column>
+      </Grid>
+        
         </div>
     )
 }
